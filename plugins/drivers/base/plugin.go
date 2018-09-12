ca223a047f53212b378387f17cfd06ba8ab50a81
Nick Ethier <nethier@hashicorp.com>
2018-09-12 15:31:56 -0400

    base/driver: remove unused code

--- plugins/drivers/base/plugin.go
+++ plugins/drivers/base/plugin.go
@@ -8,30 +8,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func LaunchDriver(d DriverPlugin) error {
-	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: plugin.HandshakeConfig{},
-		Plugins: map[string]plugin.Plugin{
-			DriverGoPlugin: &PluginDriver{impl: d},
-		},
-		GRPCServer: plugin.DefaultGRPCServer,
-	})
-	return nil
-}
-
-type ServeConfig struct {
-	EventRecorderRPCAddr string
-	HandshakeConfig      *plugin.HandshakeConfig
-	GRPCServer           func([]grpc.ServerOption) *grpc.Server
-}
-
-func Serve(cfg ServeConfig) error {
-
-	return nil
-}
-
-type DriverFactory func(interface{}) DriverPlugin
-
 // PluginDriver wraps a DriverPlugin and implements go-plugins GRPCPlugin
 // interface to expose the the interface over gRPC
 type PluginDriver struct {
